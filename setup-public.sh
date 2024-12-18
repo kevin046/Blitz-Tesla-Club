6c41816157276561a82bc6b01c8cb8156487e2d0
kevin046 <85421994+kevin046@users.noreply.github.com>
2024-12-17 22:06:36 -0500

    Initial commit: Blitz Tesla Club website

---
+++ setup-public.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create directory structure
+mkdir -p public/css public/js
+
+# Move files to their correct locations
+mv styles.css public/css/
+mv script.js public/js/
+mv register.js public/js/
+mv login.js public/js/
+mv dashboard.js public/js/
+
+# Move HTML files to public directory
+mv *.html public/ 
